d7e1fa17e3eadb571070811d1069d66ecefb3dec
akatief <akatief@users.noreply.github.com>
2022-11-01 22:34:39 +0000

    jekyll build from Action 27ba39eed2c384964251383d9f4e5493533b5b69

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Edge Case: Many Tags",
+        "excerpt":"This post has many tags.   This is an update to see if the date changes  ","categories": ["Edge Case"],
+        "tags": ["8BIT","alignment","Articles","captions","categories","chat","comments","content","css","dowork","edge case","embeds","excerpt","Fail","featured image","FTW","Fun","gallery","html","image","Jekyll","layout","link","Love","markup","Mothership","Must Read","Nailed It","Pictures","Post Formats","quote","standard","Success","Swagger","Tags","template","title","twitter","Unseen","video","YouTube","U:R:COOL","C#","Random tag to test"],
+        "url": "/the-quantumist/edge%20case/2016/07/20/many-tags.html",
+        "teaser": null
+      },{
+        "title": "Jekyll Features: Last Modified At",
+        "excerpt":"This post shows how to use the ‘last modified at’ field.  ","categories": ["Jekyll Features"],
+        "tags": ["Template"],
+        "url": "/the-quantumist/jekyll%20features/2018/06/23/last-updated-at.html",
+        "teaser": null
+      },{
+        "title": "Implementing k-Nearest Neighbors from scratch - The naive way",
+        "excerpt":"Recently I started looking for a job in the field of data science and in order to practice with possible machine learning related questions I decided to implement the most common algorithms from scratch using only numpy. In the following weeks I would like to share these implementation in my...","categories": [],
+        "tags": ["machine learning","algorithms"],
+        "url": "/the-quantumist/2022/01/27/Implementing-k-Nearest-Neighbors-from-scratch-The-naive-way.html",
+        "teaser": null
+      },{
+        "title": "Quarto Test",
+        "excerpt":"Quarto Basics   For a demonstration of a line plot on a polar axis, see Figure 1.     Figure 1: A line plot on a polar axis   This is a qubit: \\(\\ket{0}\\)   \\[\\ket{0} = \\begin{bmatrix} 1 \\\\ 0 \\end{bmatrix}\\;      \\ket{1} = \\begin{bmatrix} 0 \\\\ 1 \\end{bmatrix}\\] ","categories": [],
+        "tags": ["Template"],
+        "url": "/the-quantumist/2022/05/15/Quarto-Test.html",
+        "teaser": null
+      },{
+        "title": "Quantum Computing for Computer Engineers",
+        "excerpt":"Here’s the thing about Quantum Computing: it is one of the most interesting and promising fields of research, but at the same time one of the most obscure. The idea of building a computer using the sometimes counter-intuitive laws of quantum mechanics is quite old, and since the 80s people...","categories": [],
+        "tags": ["Quantum Computing","Linear Algebra"],
+        "url": "/the-quantumist/2022/11/01/quantum-computing-for-computer-engineers-part-1.html",
+        "teaser": null
+      }]
